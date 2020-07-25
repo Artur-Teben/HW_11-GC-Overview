d6094c03089bf6269e66212791f3cdc6e8a7f41b
Artur Teben <teben.artur99@gmail.com>
2020-07-25 19:37:23 +0300

    Create object exe for class Executor and call method startApplication

---
+++ src/com/java/error/Main.java
@@ -0,0 +1,11 @@
+package com.java.error;
+
+import com.java.error.executor.Executor;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Executor exe = new Executor();
+        exe.startApplication();
+    }
+}
