d76a0dd35e2092b905994bf56ab2e6a4abd68457
Artur Teben <teben.artur99@gmail.com>
2020-07-26 11:34:31 +0300

    Reduce numbers of lines to one

---
+++ src/com/java/cursor/teben/error/Main.java
@@ -0,0 +1,10 @@
+package com.java.cursor.teben.error;
+
+import com.java.cursor.teben.error.executor.Executor;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Executor.startApplication();
+    }
+}
