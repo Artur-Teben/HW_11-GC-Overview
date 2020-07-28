15aa715db018a2debcefe91f0d7103d3ce166ec9
Artur Teben <teben.artur99@gmail.com>
2020-07-28 18:05:24 +0300

    Call method startApplication by object exe

---
+++ src/main/java/teben/Main.java
@@ -0,0 +1,11 @@
+package main.java.teben;
+
+import main.java.teben.executor.Executor;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Executor exe = new Executor();
+        exe.startApplication();
+    }
+}
